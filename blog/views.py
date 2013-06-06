3ac5b5e9586d442001d63364f6f4ae94dab2c0e4
NASSIM <nassim.benharrat@gmail.com>
2013-06-06 10:27:07 +0200

    commit done

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+#-*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+
+def home(request):
+  text = """<h1>APPLICATION TEST</h1>
+            <p>MA PREMIERE PAGE HTML SOUS PYTHON/DJANGO!</p>"""
+
+
+  return HttpResponse(text)
+
+
+
