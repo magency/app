6462ae91b09b7a90d47639cf8a9f55fa0787ca24
NASSIM <nassim.benharrat@gmail.com>
2013-06-07 10:57:21 +0200

    COMMIT VERSION 1 PROJECT

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, url
    
-urlpatterns = patterns('',
-                       url(r'', 'blog.views.home')
+urlpatterns = patterns('blog.views',                     
+                       url(r'^welcome/$', 'home'),
+                       url(r'^data/$', 'data'),
+                       url(r'^object/(?P<id_object>\d+)/$', 'view_object'), #object view
+                       url(r'^object/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'view_object_date'), #object view with view_object_date
                        )
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,28 @@
 # Create your views here.
 #-*- coding: utf-8 -*-
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 def home(request):
-  text = """<h1>APPLICATION TEST</h1>
-            <p>MA PREMIERE PAGE HTML SOUS PYTHON/DJANGO!</p>"""
-
-
+  text = """<h1>WELCOME PAGE</h1>
+            <p>MA PREMIERE PAGE HTML SOUS PYTHON/DJANGO!</p>
+            <a href="http://localhost:8000/data/" target="_blank">TO DATA PAGE</a>"""
   return HttpResponse(text)
 
+def data(request):
+  text = """<h1>DATA PAGE</h1>
+            <href>MA PREMIERE PAGE HTML SOUS PYTHON/DJANGO!</p>
+            <a href="http://localhost:8000/welcome/" target="_blank">TO WELCOME PAGE </a>"""
+  return HttpResponse(text)
 
+def view_object(request, id_object):
+  #VIEW WHICH RETURNS AN OBJECT NUMBER
+  if not(int(id_object)): #Si l'ID est supérieur à 100, nous considérons que l'article n'existe pas
+        raise Http404
+  text = "<p>Vous avez demandé l'object n°{0} !</p>".format(id_object)
+  return HttpResponse(text)
 
+def view_object_date(request, day, month, year):
+  #Liste des articles d'un mois précis
+  text = "Vous avez demandé l'object de la date {0}/{1}/{2}.".format(day, month, year)
+  return HttpResponse(text)  
