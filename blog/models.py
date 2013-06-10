7d6d520d18a020a19f50e150c182ce73fe38e912
NASSIM <nassim.benharrat@gmail.com>
2013-06-10 17:16:32 +0200

    COMMIT OF MONDAY JUNE 10TH

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
+#-*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+ 
+class MessageCategory(models.Model):
+    name = models.CharField(max_length=30)
+ 
+    def __unicode__(self):
+        return self.nom
+
+class Message(models.Model):
+	title = models.CharField(max_length=100)
+   	user = models.CharField(max_length=42)
+   	content = models.TextField(null=True)
+   	date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Post date")
+ 	category = models.ForeignKey('MessageCategory')
+ 	
+   	def __unicode__(self):
+   	    return u"%s" % self.title
+
